dfcea0c36fd7006865759c99b58ddfe6833b7381
Andreamg1 <145478212+Andreamg1@users.noreply.github.com>
2026-03-17 19:17:15 +0400

    Remove token from file, load from environment

--- MapboxAPI.py
+++ MapboxAPI.py
@@ -1,79 +1,13 @@
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from pydantic import BaseModel
-# import requests
 
-# app = FastAPI(title="Mapbox Driving-Traffic API")
-
-# # ✅ CORS (allows React frontend to call API)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:5173"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# # 🔐 Your Mapbox token
-# MAPBOX_TOKEN = "your_mapbox_token_here"
-
-
-# # 📍 Data Models
-# class Coordinates(BaseModel):
-#     lat: float
-#     lon: float
-
-
-# class RouteRequest(BaseModel):
-#     source: Coordinates
-#     destination: Coordinates
-
-
-# # 🚀 API Endpoint
-# @app.post("/get_routes")
-# def get_routes_api(req: RouteRequest):
-#     src_lat, src_lon = req.source.lat, req.source.lon
-#     dest_lat, dest_lon = req.destination.lat, req.destination.lon
-
-#     url = f"https://api.mapbox.com/directions/v5/mapbox/driving-traffic/{src_lon},{src_lat};{dest_lon},{dest_lat}"
-
-#     params = {
-#         "alternatives": "true",
-#         "geometries": "geojson",
-#         "overview": "full",
-#         "access_token": MAPBOX_TOKEN
-#     }
-
-#     response = requests.get(url, params=params)
-#     data = response.json()
-
-#     # 🔍 Debug (optional)
-#     print("Mapbox response:", data)
-
-#     routes = data.get("routes", [])
-
-#     result = []
-#     for i, r in enumerate(routes[:5]):
-#         distance_km = r["distance"] / 1000
-#         duration_min = r["duration"] / 60
-#         coordinates = r["geometry"]["coordinates"]
-
-#         result.append({
-#             "route_number": i + 1,
-#             "distance_km": round(distance_km, 2),
-#             "duration_min": round(duration_min, 2),
-#             "num_points": len(coordinates),
-#             "coordinates": coordinates   # ✅ FULL route for drawing
-#         })
-
-#     return {
-#         "num_routes": len(result),
-#         "routes": result
-#     }
+from dotenv import load_dotenv
+load_dotenv()
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import os
+
+VITE_TOKEN = os.getenv("MAPBOX_TOKEN")
 import requests
 
 app = FastAPI(title="Mapbox Driving-Traffic API")
@@ -87,10 +21,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 🔐 Your Mapbox token
-MAPBOX_TOKEN = "pk.eyJ1IjoiYW1nMjAyNiIsImEiOiJjbW1uNHV4bGUwbGRqMnJxdjJhdDdrZmQzIn0.m1gF3QR6Sa5_XRzcn0h5hg"
-
-# 📍 Data Models
 class Coordinates(BaseModel):
     lat: float
     lon: float
@@ -101,7 +31,6 @@ class RouteRequest(BaseModel):
     destination: Coordinates
 
 
-# 🚀 API Endpoint
 @app.post("/get_routes")
 def get_routes_api(req: RouteRequest):
     src_lat, src_lon = req.source.lat, req.source.lon
